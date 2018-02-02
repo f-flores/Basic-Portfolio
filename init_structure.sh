e808d81f436b3fc50c9b7cbf5a5b27ff4559b0c5
Fabian Flores <ffflores1@outlook.com>
2018-02-01 23:10:50 -0500

    Removed some unnecessary files.

--- init_structure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-touch index.html contact.html portfolio.html
-mkdir -vp assets
-mkdir -vp assets/css
-mkdir -vp assets/images
-touch assets/css/style.css
-touch assets/css/reset.css
-touch assets/images/example.jpg
